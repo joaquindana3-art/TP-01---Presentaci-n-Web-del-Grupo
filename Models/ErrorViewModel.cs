d6be626abdfadfbab77a1cf445412db606686316
JOAQUIN URIEL DANA <50705111@ort>
2026-04-17 07:57:35 -0300

    First commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace TP01_Dana_Grinblat_Poniachik.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
